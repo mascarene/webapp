bf869c4fd568e0ff9fd9cf7fd4674a15d2d39b09
mascarene <quentin@outlook.sg>
2023-01-16 12:13:14 +0100

    add wasm-pack-plugin and register with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin')
+const { resolve } = require('path')
 
 module.exports = {
     entry: './public/main.js',
@@ -10,6 +12,9 @@ module.exports = {
     plugins: [
         new HTMLWebpackPlugin({
             template: './public/index.html'
+        }),
+        new WasmPackPlugin({
+            crateDirectory: path.resolve(__dirname, '.')
         })
     ]
 }
