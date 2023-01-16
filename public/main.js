68eb2913629bd51ee9147c21cc3920334a7e3bc0
mascarene <quentin@outlook.sg>
2023-01-16 11:42:41 +0100

    debug read file data

--- public/main.js
+++ public/main.js
@@ -2,8 +2,14 @@ function init() {
     const input = document.getElementById('upload')
     const fileReader = new fileReader()
 
-    input.addEventListener('change', () => {
+    fileReader.onloadend = () => {
+        let base64 = fileReader.result
+        console.log(input.files[0])
+        console.log(base64)
+    }
 
+    input.addEventListener('change', () => {
+        fileReader.readAsDataURL(input.files[0])
     })
 }
 
