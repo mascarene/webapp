f638884555f43ceaa201eb79a9d869abdb4defe4
mascarene <quentin@outlook.sg>
2023-01-16 11:59:59 +0100

    regex for removing metadata before transfert

--- public/main.js
+++ public/main.js
@@ -3,7 +3,10 @@ function init() {
     const fileReader = new FileReader()
 
     fileReader.onloadend = () => {
-        let base64 = fileReader.result
+        let base64 = fileReader.result.replace(
+            // Search for the metadata prefix and replace it with an empty string
+            /^data:image\/(png|jpeg|jpg);base64,/, ''
+        )
         console.log(input.files[0])
         console.log(base64)
     }
